74875a03bda88b8e9a9e774fcf583b33981d2bd3
Arnold Kom <akom@autoroot.io>
2022-10-25 19:51:08 +0100

    update

--- src/stripe/initializeStripe.ts
+++ src/stripe/initializeStripe.ts
@@ -1,7 +1,7 @@
 import { Stripe, loadStripe } from "@stripe/stripe-js";
 
 let stripePromise: Stripe | null;
-
+ 
 const initializeStripe = async () => {
   if (!stripePromise) {
     stripePromise = await loadStripe(
